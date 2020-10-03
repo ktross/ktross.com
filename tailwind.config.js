06e0504f429a1594bba1a79ef24c3b3b722d20a5
Kyle Ross <kyle@ktross.com>
2020-10-03 03:14:23 -0400

    Add tailwindcss (#1)
    * Update .gitignore
    
    * Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+      'resources/**/*.js',
+      'resources/**/*.ts',
+      'resources/**/*.php',
+      'resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
