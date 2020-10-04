adf19b977f28e829767e5de2b102512122d1860e
Kyle Ross <kyle@ktross.com>
2020-10-04 00:32:24 -0400

    add fontawesome (#3)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/ec6bb0f298.js" crossorigin="anonymous"></script>
 
         <!-- Styles -->
         <style>
